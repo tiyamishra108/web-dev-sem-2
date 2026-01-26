4397c5a2644452a7f669be9f4fba5479daceb4ba
Tiya Mishra <tiyamishra2007@gmail.com>
2026-01-26 15:48:03 +0530

    Add files via upload

---
+++ JsDay 4/script.js
@@ -0,0 +1,9 @@
+//-------------------------------OBJECT IN JAVASCRIPT----------------------------------//
+let user = {
+    fullname:"tiya mishra",
+    address:"gurugram",
+    numner:9876543234,
+    favcolour:["black","blue","white"],}
+//we can acess the individual value by targeting key with variable name
+console.log(user.fullname,user.address,user.numner,user.favcolour[1]);
+//output:tiya mishra gurugram 9876543234 blue
