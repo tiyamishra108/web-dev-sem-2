fd172d69967b93dc32ed7c82bf72e9adc59148d2
Tiya Mishra <tiyamishra2007@gmail.com>
2026-01-29 11:54:01 +0530

    Add files via upload

---
+++ JsDay 7/script.js
@@ -0,0 +1,13 @@
+//----------------Task 1----------------//
+const profile=document.getElementById('profile');
+profile.style.backgroundColor='lightblue';
+profile.style.padding='15px';
+profile.style.textAlign='center';
+
+//----------------Task 2----------------//
+const highlight=document.getElementsByClassName('important');
+console.log(highlight)
+for(let i=0;i<highlight.length;i++){
+    highlight[i].style.color='red';
+    highlight[i].style.fontSize='15px';
+}
