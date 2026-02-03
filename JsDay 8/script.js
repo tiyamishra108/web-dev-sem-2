a0ccae82bfae7a398e1714f90106203df28ce97a
Tiya Mishra <tiyamishra2007@gmail.com>
2026-02-03 12:37:15 +0530

    Add files via upload

---
+++ JsDay 8/script.js
@@ -0,0 +1,45 @@
+//----------------Task 1----------------//
+// const profile=document.getElementById('profile');
+// profile.style.backgroundColor='lightblue';
+// profile.style.padding='15px';
+// profile.style.textAlign='center';
+
+//----------------Task 2----------------//
+// const highlight=document.getElementsByClassName('important');
+// console.log(highlight)
+// for(let i=0;i<highlight.length;i++){
+//     highlight[i].style.color='red';
+//     highlight[i].style.fontSize='15px';
+// }
+
+//----------------Task 3----------------//
+// const task3=document.queryselectortagname('p');
+// console.log(task3);
+// for(let i=0;i<task3.length;i++){
+//     if(i%2===0){
+//         task3[i].style.color = "red";
+//     } else {
+//          task3[i].style.color = "blue";
+//     }
+// }
+// task3[task3.length-1].style.fontWeight = "bold";
+
+//----------------Task 4----------------//
+// const task4=document.querySelectorAll('.item');
+// for(let i=0;i<task4.length;i++){
+//     task4[i].style.backgroundColor='lightgray';
+// }
+// task4[0].textContent="i am the first box";
+
+//----------------Task 5----------------//
+const section=document.getElementById('content');
+const task5=section.querySelectorAll('p');
+for(let i=0;i<task5.length;i++){
+task5[i].style.color='green';
+}
+const section2=document.getElementById('guest')
+
+
+        
+    
+
